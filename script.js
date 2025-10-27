66889b776ec3ba809feba82c510b0fcfc0a74541
iAliSalem <alisalemgamil2@gmail.com>
2025-10-27 16:27:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ particles.push({
   y: Math.random() * canvas.height,
   size: Math.random() * 2 + 1,
   speedX: (Math.random() - 0.5) * 0.1,
-  speedY: (Math.random() - 0.5) * 0.1,
+  speedY = Math.random() * 0.4 + 0.1;
   opacity: Math.random()
 });
 
@@ -49,3 +49,51 @@ function animate() {
 }
 
 animate();
+// --- Add this at the end of script.js ---
+
+let tiltX = 0;
+let tiltY = 0;
+
+if (window.DeviceOrientationEvent) {
+  window.addEventListener("deviceorientation", (event) => {
+    // event.beta = front/back, event.gamma = left/right tilt
+    tiltX = event.gamma / 50; 
+    tiltY = event.beta / 50;
+  });
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "rgba(0, 255, 153, 0.8)";
+  particles.forEach((p) => {
+    p.update();
+    p.draw();
+  });
+  requestAnimationFrame(draw);
+}
+
+class Particle {
+  constructor() {
+    this.reset();
+  }
+  reset() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 2 + 1;
+    this.speedY = Math.random() * 0.4 + 0.1;
+    this.alpha = Math.random() * 0.5 + 0.5;
+  }
+  update() {
+    this.y += this.speedY + tiltY * 0.5;
+    this.x += tiltX * 0.5;
+    if (this.y > canvas.height) this.reset();
+    if (this.x > canvas.width) this.x = 0;
+    if (this.x < 0) this.x = canvas.width;
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fillStyle = `rgba(0, 255, 153, ${this.alpha})`;
+    ctx.fill();
+  }
+}
